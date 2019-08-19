ad2969017792aafba859afcf466c28cccf03aaf1
LE MENTEC Mikaël <mle-mentec@ac-orleans-tours.fr>
2019-08-19 14:33:34 +0200

    Augmentation des tailles maximales

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Geogebra(models.Model):
     id_geogebra = models.CharField(max_length=50)
     titre = models.CharField(max_length=100)
-    width = models.IntegerField(default=800,validators=[MaxValueValidator(1000),MinValueValidator(1)])
-    height = models.IntegerField(default=600,validators=[MaxValueValidator(1000),MinValueValidator(1)])
+    width = models.IntegerField(default=800,validators=[MaxValueValidator(10000),MinValueValidator(1)])
+    height = models.IntegerField(default=600,validators=[MaxValueValidator(10000),MinValueValidator(1)])
 
     def __str__(self):
         return self.titre
